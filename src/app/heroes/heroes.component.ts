160917cb70a645d5b2744bcab219754a9a804316
MaheshSuranga <mahesh.15@cse.mrt.ac.lk>
2018-03-16 04:29:19 +0530

    Create heroService and messageService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,7 +8,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes = HEROES;
+  heroes;
   selectedHero: Hero;
 
   onSelect(hero: Hero): void {
@@ -20,9 +20,14 @@ export class HeroesComponent implements OnInit {
     name: 'Windstorm',
   }
 
-  constructor() { }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit() {
+    this.getHeroes();
   }
 
 }
