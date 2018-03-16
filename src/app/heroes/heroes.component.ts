94503756bee0713b5b3647a3f53fc9c2bf8c5b62
MaheshSuranga <mahesh.15@cse.mrt.ac.lk>
2018-03-16 17:20:23 +0530

    Routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,17 +8,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  heroes;
-  selectedHero: Hero;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm',
-  }
+  heroes: Hero[];
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
